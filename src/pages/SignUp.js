75aa62991cc2c227fb0f239811ed429507c7e5cc
mdhudamia <hudamiakm@gmail.com>
2023-01-01 23:53:03 +0600

    register

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import Helmet from '../componanet/CommonSection/Helmet';
 import { AuthContext } from '../Context/AuthProvider';
 
 const SignUp = () => {
@@ -43,44 +44,46 @@ const SignUp = () => {
 
 
     return (
-        <div className="hero min-h-screen py-10">
-            <div className="card  w-96 shadow-2xl bg-base-100">
-                <div className="card-body">
-                    <h1 className="text-3xl text-center mb-4 font-bold">Register</h1>
-                    <form onSubmit={handleSignUp}>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Name</span>
-                            </label>
-                            <input type="text" name='name' placeholder="name" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="/" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <input type="submit" value="Sing Up" className="btn btn-primary" />
-                        </div>
-                    </form>
+        <Helmet title={'Register'}>
+            <div className="hero min-h-screen py-10">
+                <div className="card  w-96 shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-3xl text-center mb-4 font-bold">Register</h1>
+                        <form onSubmit={handleSignUp}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="/" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" value="Sing Up" className="btn btn-primary" />
+                            </div>
+                        </form>
 
-                    <div className="form-control mt-6">
-                        <p className='text-sm'>Already Have an account <Link className='font-semibold hover:underline ' to={"/login"}>Login</Link> </p>
+                        <div className="form-control mt-6">
+                            <p className='text-sm'>Already Have an account <Link className='font-semibold hover:underline ' to={"/login"}>Login</Link> </p>
+                        </div>
+                        <button onClick={handleGoogleSingIn} className="mt-7 "> <img className='w-5 h-5 text-center inline-block' src="https://freesvg.org/img/1534129544.png" alt="" /> <span className='font-semibold'>Google</span></button>
                     </div>
-                    <button onClick={handleGoogleSingIn} className="mt-7 "> <img className='w-5 h-5 text-center inline-block' src="https://freesvg.org/img/1534129544.png" alt="" /> <span className='font-semibold'>Google</span></button>
                 </div>
             </div>
-        </div>
+        </Helmet>
     );
 };
 

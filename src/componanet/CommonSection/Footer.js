ebfcb15051698e1999233dfc732f2a42efd7ad46
mdhudamia <hudamiakm@gmail.com>
2023-01-01 23:15:41 +0600

    footer

--- src/componanet/CommonSection/Footer.js
+++ src/componanet/CommonSection/Footer.js
@@ -5,16 +5,16 @@ const Footer = () => {
         <footer className="footer p-10 bg-base-200 text-base-content">
             <div>
                 <span className="footer-title">Services</span>
-                <a href="/" className="link link-hover">Branding</a>
-                <a href="/" className="link link-hover">Design</a>
-                <a href="/" className="link link-hover">Marketing</a>
-                <a href="/" className="link link-hover">Advertisement</a>
+                <a href="/" className="link link-hover">London</a>
+                <a href="/" className="link link-hover">America</a>
+                <a href="/" className="link link-hover">parrice</a>
+                <a href="/" className="link link-hover">Bandhorbaan</a>
             </div>
             <div>
                 <span className="footer-title">Company</span>
                 <a href="/" className="link link-hover">About us</a>
                 <a href="/" className="link link-hover">Contact</a>
-                <a href="/" className="link link-hover">Jobs</a>
+                <a href="/" className="link link-hover">Blog</a>
                 <a href="/" className="link link-hover">Press kit</a>
             </div>
             <div>
